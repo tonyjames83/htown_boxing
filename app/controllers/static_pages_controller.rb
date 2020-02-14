42f079f76aaf6c8cd0af9b5bbf3fbb04ce1007b1
Tony James <jjames83@gmail.com>
2020-02-14 15:38:11 -0600

    got basic content in place need to work on styling and adding photos

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def faq
+  end
+
+  def home
+  end
+
+  def staff
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get static_pages_faq_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get staff" do
+    get static_pages_staff_url
+    assert_response :success
+  end
+
+end
